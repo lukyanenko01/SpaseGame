2ab9bdf5603dac972a4426b5609d377524003a64
Vladimir Lukyanenko <>
2023-03-02 20:54:03 +0200

    create spaceShip

--- SpaceGame/GameScene.swift
+++ SpaceGame/GameScene.swift
@@ -10,11 +10,31 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var spaceShip: SKSpriteNode!
+    
     override func didMove(to view: SKView) {
-        let spaceShip = SKSpriteNode(imageNamed: "spaceShip")
-        
-//        spaceShip.position = CGPoint(x: 200, y: 200)
-        
+        spaceShip = SKSpriteNode(imageNamed: "spaceShip")
         addChild(spaceShip)
+        
+        let width = UIScreen.main.bounds.size.width
+        let height = UIScreen.main.bounds.size.height
+        
+        let background = SKSpriteNode(imageNamed: "background")
+        background.size = CGSize(width: width, height: height)
+        addChild(background)
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            
+            let touchLocation = touch.location(in: self)
+            
+            let moveAction = SKAction.move(to: touchLocation, duration: 1.0)
+            spaceShip.run(moveAction)
+            
+        }
+    }
+    
+    
+    
 }
